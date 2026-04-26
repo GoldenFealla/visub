d853925901223682c3f0422a7965db916c25139a
Goldenfealla <telisacaramore@gmail.com>
2026-04-26 12:36:01 +0700

    feat: add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from "node:path";
+
 import { defineConfig } from "vite";
 
 import vue from "@vitejs/plugin-vue";
@@ -6,4 +8,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
